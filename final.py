2c51e1dc642a149a209da622762ee961846f26f9
Rowan Mankoo <Rowan.mankoo@users.noreply.github.com>
2022-04-25 20:53:19 +0100

    Initialised repo

---
+++ final.py
@@ -0,0 +1,121 @@
+import datetime
+import math
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xgboost as xgboost
+from sklearn.metrics import mean_squared_error, accuracy_score
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from xgboost import XGBRegressor
+
+# add feature importance 
+# only sell if probability is high
+# fixed training window
+# go backwards
+
+
+class Model_Dev:
+    def __init__(self, X, Y, Y_closing_prices,train_test_split=0.8):
+        self.prediction_window = 5 # fix this!
+        self.X = X
+        self.Y = Y
+        self.Y_closing_prices = Y_closing_prices
+        self.n = int(self.X.shape[0]*train_test_split)
+
+        self.stocks = 5
+        self.money = 0
+
+    def walkthrough_train_test_split(self, i):
+        # Return X_train, Y_train, X_test, Y_test, Y_closing_pries_test
+        return self.X[:self.n+i,:], self.Y[:self.n+i], self.X[self.n+i:,:], self.Y[self.n+i:], self.Y_closing_prices[self.n+i:]
+
+    def validate(self):
+        # Hyperparameter tunning
+        # chnage this to do all but last 5?
+        X_train, Y_train, _,_,_ = self.walkthrough_train_test_split(0)         
+        params = {
+                'min_child_weight': [1, 5, 10],
+                'gamma': [0.5, 1, 1.5, 2, 5],
+                'subsample': [0.6, 0.8, 1.0],
+                'colsample_bytree': [0.6, 0.8, 1.0],
+                'max_depth': [3, 4, 5]
+                }
+
+        xgb1 = xgboost.XGBClassifier()
+        xgb_grid = GridSearchCV(xgb1,
+                                params,
+                                cv = TimeSeriesSplit(),
+                                n_jobs = -1,
+                                verbose=True)
+        xgb_grid.fit(X_train,Y_train)
+        self.best_params = xgb_grid.best_params_
+    
+    def train_and_predict_step(self, X_train, Y_train, X_test, Y_test, Y_closing_prices_test):
+        
+        model = XGBRegressor(**self.best_params)
+        model.fit(X_train, Y_train)
+
+        BinaryPredicted = model.predict(X_test[0].reshape(-1,X_test.shape[1]))
+        BinaryActual = Y_test[0]  
+        ActualStockClosingPrice = Y_closing_prices_test[0]
+
+        return BinaryPredicted, BinaryActual, ActualStockClosingPrice
+    
+    def money_simulation(self, BinaryPredicted, ActualStockClosingPrice):
+
+        if BinaryPredicted==0:
+            if self.stocks!=0:
+                # If stocks are predicted to go down then sell
+                self.money += ActualStockClosingPrice*stocks
+                self.stocks = 0
+            else:
+                pass
+        else:
+            # If stocks are predicted to go up then buy
+            self.stocks += self.money//ActualStockClosingPrice
+            self.money -= (self.money//ActualStockClosingPrice)*ActualStockClosingPrice
+
+    def visualise(self):
+        
+        plt.plot(list(pred)*(prediction_window+1),color='red',label='Predictions')
+        plt.plot([starting_price]+list(Y_closing_prices[max(i,0):max(i,0)+prediction_window]),color='blue',label='Actual')
+        plt.legend()
+        plt.show()
+    
+    def print_money_gain():
+        print('f')
+    
+    def print_accuracy(self):
+
+        preds = list(map(lambda x: 1 if x>0.5 else 0, self.preds))
+        print(accuracy_score(self.acc,preds))
+
+    def simulation_walkthrough(self):
+
+        self.starting_money = self.money + self.stocks*self.X[self.n,3]
+
+        self.preds = []
+        self.acc = []
+        for i in range(0,self.X.shape[0]-self.n,self.prediction_window):
+
+            X_train, Y_train, X_test, Y_test, Y_closing_pries_test = self.walkthrough_train_test_split(i)
+            BinaryPredicted, BinaryActual, ActualStockClosingPrice = self.train_and_predict_step(X_train, Y_train, X_test, Y_test, Y_closing_pries_test)
+            # classification error:
+            self.preds.append(BinaryPredicted)
+            self.acc.append(BinaryActual)
+
+            self.money_simulation(BinaryPredicted, ActualStockClosingPrice)
+
+        self.ending_money = self.money + self.stocks*self.Y_closing_prices[-1]
+
+        print(f'The starting Money was {self.starting_money} and the ending money was {self.ending_money} so the total profit would have been: £{self.ending_money-self.starting_money}')
+        self.print_accuracy()
+
+
+
+X, Y, Y_closing_prices = data_cleaning_pipeline(df, =5)
+
+model = Model_Dev(X,Y,Y_closing_prices)
+model.validate()
