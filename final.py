b75dc5ce10139de9a7ff554bf97d29e3b7fd14fb
Rowan Mankoo <Rowan.mankoo@users.noreply.github.com>
2022-05-15 20:48:36 +0100

    added inhertience to ModelDev from Data class and added in probability visualsation methods

--- final.py
+++ final.py
@@ -9,6 +9,8 @@ from sklearn.metrics import accuracy_score, mean_squared_error
 from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
 from xgboost import XGBClassifier
 
+import data
+
 # develop feature importance functionality
 # only sell if probability is high
 # fixed training window
@@ -19,19 +21,25 @@ from xgboost import XGBClassifier
 # Try new hyper parameter optimisation?
 # double check money simulation walkthrough
 ### include last 30 days of high?
+# Venv
+# progress bars or print init statements
 
-class Model_Dev:
-    def __init__(self, X, Y, Y_closing_prices, X_current, train_test_split=0.8, hyperparams=None):
-        self.prediction_window = 5 # fix this!
-        self.X = X
-        self.Y = Y
-        self.Y_closing_prices = Y_closing_prices
-        self.X_current = X_current.reshape(-1,X_current.shape[0])
+class ModelDev(data.Data):
+    def __init__(self, company, steps, train_test_split=0.8, hyperparams=None):
+
+        # Initialise data class and prepare data for model
+        self.steps = steps
+        super().__init__(company, steps)
+        self.X, self.Y, self.Y_closing_prices, self.X_current = self.X_Y_dataset_creation()
+        self.X_current = self.X_current.reshape(-1,self.X_current.shape[0])
         self.n = int(self.X.shape[0]*train_test_split)
-
-        counter = Counter(Y)
+       
+        # Used for class balancing 
+        counter = Counter(self.Y)
         self.weighting = counter[0]/counter[1]
 
+        # Used for money simulation
+        # MOVE THIS TO MONEY SIM METHOD
         self.stocks = 5
         self.money = 0
 
@@ -81,7 +89,7 @@ class Model_Dev:
 
         return BinaryPredicted, BinaryActual, ActualStockClosingPrice, prob
     
-    def money_simulation(self, BinaryPredicted, ActualStockClosingPrice):
+    def __money_simulation(self, BinaryPredicted, ActualStockClosingPrice):
 
         if BinaryPredicted==0:
             if self.stocks!=0:
@@ -110,14 +118,6 @@ class Model_Dev:
         plt.plot([starting_price]+list(Y_closing_prices[max(i,0):max(i,0)+prediction_window]),color='blue',label='Actual')
         plt.legend()
         plt.show()
-    
-    def print_money_gain():
-        print('f')
-    
-    def print_accuracy(self):
-
-        #preds = list(map(lambda x: 1 if x>0.5 else 0, self.preds))
-        print(accuracy_score(self.acc,self.preds))
 
     def simulation_walkthrough(self):
 
@@ -135,12 +135,72 @@ class Model_Dev:
             self.acc.append(BinaryActual)
             self.probs.append(prob)
 
-            self.money_simulation(BinaryPredicted, ActualStockClosingPrice)
+            self.__money_simulation(BinaryPredicted, ActualStockClosingPrice)
 
         self.ending_money = self.money + self.stocks*self.Y_closing_prices[-1]
 
         print(f'The starting Money was {self.starting_money} and the ending money was {self.ending_money} so the total profit would have been: £{self.ending_money-self.starting_money}')
-        self.print_accuracy()
+        print(accuracy_score(self.acc,self.preds))
+
+        self.__simulation_walkthrough_has_been_called__ = True
+
+    def visualise_correct_incorrect_probs(self):
+
+        correct = []
+        incorrect = []
+        for acc,probs in zip(self.acc,self.probs):
+            if acc==1 and probs[0,1]>0.5:
+                correct.append(probs[0,1])
+            elif acc==0 and probs[0,1]<0.5:
+                correct.append(probs[0,1])
+            else:
+                incorrect.append(probs[0,1])
+
+        plt.style.use('seaborn')
+        plt.boxplot(correct+incorrect, showfliers=False)
+
+        data = [correct,incorrect]
+        for dataset in data:
+            if dataset == correct:
+                color = 'blue'
+                label = 'Correct'
+            else:
+                color = 'red'
+                label = 'Incorrect'
+            y = dataset
+            x = np.random.normal(1, 0.03, len(y))
+            plt.scatter(x,y,color=color, label=label)
+
+        plt.xticks([1],[''])
+        plt.title('Distribution of both correct/incorrect predicted probabilities')
+        plt.legend()
+        plt.ylabel('Predicted Probability of increasing')
+        plt.show()
+
+    def visualise_class_probs(self):
+
+        increased = []
+        decreased = []
+        for acc,probs in zip(self.acc,self.probs):
+            if acc==1:
+                increased.append(probs[0,1])
+            else:
+                decreased.append(probs[0,1])
+
+        plt.style.use('seaborn')
+        data = [increased,decreased]
+        plt.boxplot(data, showfliers=False)
+
+        for i,dataset in enumerate(data):
+            y = dataset
+            x = np.random.normal(i + 1, 0.04, len(y))
+            plt.scatter(x,y)
+
+        plt.xticks([1,2],['Increasing','Decreasing'])
+        plt.title('Predicted probability distributions of each class')
+        plt.xlabel('Actual Class')
+        plt.ylabel('Predicted Probability of increasing')
+        plt.show()
 
     def predict(self):
 
