9291a66d98b64c28bc7edffc32a34a517b55b1a6
Rowan Mankoo <Rowan.mankoo@users.noreply.github.com>
2022-06-02 21:06:08 +0100

    bug fix on test_high

--- test_dev_model.py
+++ test_dev_model.py
@@ -35,8 +35,8 @@ def test_companies(company,steps,best_params):
     m.visualise_history(test=True)
 
     # Create/add to report.csv
-    if exists('Report.csv'):
-        report = pd.read_csv('Report.csv')
+    if exists('Report_1.csv'):
+        report = pd.read_csv('Report_1.csv')
     else:
         report = pd.DataFrame(columns = ['Company','Steps','Profit'])
     data = {'Company':company,
